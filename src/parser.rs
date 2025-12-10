2944cdcd213484bab301f1eeb3901c33176c5219
darrenkuro <odon5ht@gmail.com>
2025-12-10 04:47:13 +0100

    Minor fixes

--- src/parser.rs
+++ src/parser.rs
@@ -4,9 +4,13 @@ use crate::math::term::Term;
 use std::error::Error;
 
 pub fn parse(args: &str) -> Result<Polynomial, Box<dyn Error>> {
-    let args = args.trim();
+    let args = args.trim().replace("x", "X"); // Accept both x and X
+
     if args.is_empty() {
         return Err("empty input!".into());
+    }
+    if !args.contains("X") {
+        return Err("no unknown variable X (or x) to solve!".into());
     }
 
     // Divide into left and right sides
@@ -18,7 +22,7 @@ pub fn parse(args: &str) -> Result<Polynomial, Box<dyn Error>> {
     }
 
     // Reformat - sign for easy processing
-    let lhs = sides[0].replace("- ", "+ -");
+    let lhs = sides[0].replace("- ", "+ -").replace("-", "+ -");
     let terms: Vec<&str> = lhs.split('+').collect();
     if terms.is_empty() {
         return Err("syntax: left side is empty!".into());
@@ -32,7 +36,7 @@ pub fn parse(args: &str) -> Result<Polynomial, Box<dyn Error>> {
         }
     }
 
-    let rhs = sides[1].replace("- ", "+ -");
+    let rhs = sides[1].replace("- ", "+ -").replace("-", "+ -");
     let terms: Vec<&str> = rhs.split('+').collect();
     if terms.is_empty() {
         return Err("syntax: right side is empty!".into());
